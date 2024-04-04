63784493dee41e703ca0dc7dc3068945d7d88fd1
suptext <165878111+suptext@users.noreply.github.com>
2024-04-04 07:24:24 +0000

    Update config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -6,8 +6,8 @@ import * as fs from 'fs';
 const header = `
   <h1><a href="./">suptext</a></h1>
   <h2>notes and analyses</h2>
-  <hr/>
-`
+  <hr/>`
+
 // See https://observablehq.com/framework/config for documentation.
 export default {
 
@@ -36,7 +36,7 @@ export default {
      theme: ["dark","alt","sun-faded"], // try "light", "dark", "slate", etc.
      header, // what to show in the header (HTML)
      footer: "", // what to show in the footer (HTML)
-  // toc: true, // whether to show the table of contents
+     toc: false, // whether to show the table of contents
      pager: true, // whether to show previous & next links in the footer
   // root: "docs", // path to the source root for preview
   // output: "dist", // path to the output root for build
