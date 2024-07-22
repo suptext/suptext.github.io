467bedc4a12472eccab6ad7e455cff49d957d1df
suptext <165878111+suptext@users.noreply.github.com>
2024-07-22 11:27:43 +0000

    Update metadata

--- observablehq.config.js
+++ observablehq.config.js
@@ -38,16 +38,16 @@ export default {
      <!-- Facebook Meta Tags -->
      <meta property="og:url" content="https://suptext.is/">
      <meta property="og:type" content="website">
-     <meta property="og:title" content="suptext | more thought on tools, language and development">
-     <meta property="og:description" content="Research posts and articles by Dennis Leeftink">
+     <meta property="og:title" content="more thought on tools, language and development">
+     <meta property="og:description" content="| research posts and articles by Dennis Leeftink">
      <meta property="og:image" content="https://suptext.is/_file/imgs/icon.6577852d.png">
      
      <!-- Twitter Meta Tags -->
      <meta name="twitter:card" content="summary_large_image">
      <meta property="twitter:domain" content="suptext.is">
      <meta property="twitter:url" content="https://suptext.is/">
-     <meta name="twitter:title" content="suptext | more thought on tools, language and development">
-     <meta name="twitter:description" content="Research posts and articles by Dennis Leeftink">
+     <meta name="twitter:title" content="more thought on tools, language and development">
+     <meta name="twitter:description" content="| research posts and articles by Dennis Leeftink">
      <meta name="twitter:image" content="https://suptext.is/_file/imgs/icon.6577852d.png">
      
      <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
