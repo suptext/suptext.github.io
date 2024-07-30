472407e89ecf231fd840ec17c92405d84f16569e
suptext <dennisleeftink+sup@gmail.com>
2024-07-30 21:09:27 +1000

    Apply table of contents logic and styling

--- code/util/contents.js
+++ code/util/contents.js
@@ -2,18 +2,32 @@ let toggle = document.createElement('input');
 let label = document.createElement('label');
 
 
-toggle.setAttribute('type','checkbox');
-toggle.setAttribute('id','toggle');
+toggle.setAttribute('type', 'checkbox');
+toggle.setAttribute('id', 'toggle');
 toggle.classList.add('unfolder');
 
-label.setAttribute('for','toggle');
+label.setAttribute('for', 'toggle');
 label.classList.add('toggle-label');
 
 label.textContent = '≡';
 
 let toc = document.querySelector('#observablehq-toc');
-  
-  toc.insertAdjacentElement('afterbegin',label);
-  toc.insertAdjacentElement('afterbegin',toggle);
 
-document.querySelector('#observablehq-main > h2:first-of-type').insertAdjacentElement("beforeend",toc);
+toc.insertAdjacentElement('afterbegin', label);
+toc.insertAdjacentElement('afterbegin', toggle);
+
+document.querySelector('#observablehq-main > h2:first-of-type').insertAdjacentElement("afterbegin", toc);
+
+
+window.addEventListener('click', function (event) {
+    var target = event.target || event.srcElement;
+    if (target.classList.contains('unfolder') || target.classList.contains('toggle-label')) {
+      // e.preventDefault(); // Prevent the default action (navigation)
+      // var href = target.getAttribute('href');
+
+
+    } else {
+        toggle.checked = false;
+
+    }
+}, false);
