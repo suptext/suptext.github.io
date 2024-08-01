c16933962e22b28062bc41cd921f3d3f4e6ce73b
suptext <dennisleeftink+sup@gmail.com>
2024-08-01 13:47:25 +1000

    Avoid contents.js/css jumping

--- code/util/contents.js
+++ code/util/contents.js
@@ -1,3 +1,6 @@
+
+let toc = document.querySelector('#observablehq-toc');
+
 let toggle = document.createElement('input');
 let label = document.createElement('label');
 
@@ -11,21 +14,17 @@ label.classList.add('toggle-label');
 
 label.textContent = '≡';
 
-let toc = document.querySelector('#observablehq-toc');
-
 toc.insertAdjacentElement('afterbegin', label);
 toc.insertAdjacentElement('afterbegin', toggle);
 
 document.querySelector('#observablehq-main > h2:first-of-type').insertAdjacentElement("afterbegin", toc);
-
+toc.classList.add('width-adjust')
 
 window.addEventListener('click', function (event) {
     var target = event.target || event.srcElement;
     if (target.classList.contains('unfolder') || target.classList.contains('toggle-label')) {
       // e.preventDefault(); // Prevent the default action (navigation)
       // var href = target.getAttribute('href');
-
-
     } else {
         toggle.checked = false;
 
